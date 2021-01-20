df00263c7e4610b99c336ce7c694d5651340bb3f
Jonathan Borralho <jonathan.borralho@gmail.com>
2021-01-20 09:58:12 -0300

    Exclusão de faixas de cep

--- src/main/java/br/com/wine/api/controller/FaixaCepController.java
+++ src/main/java/br/com/wine/api/controller/FaixaCepController.java
@@ -7,13 +7,16 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.wine.domain.exception.EntidadeNaoEncontradaException;
 import br.com.wine.domain.exception.FaixaCepConflitoException;
 import br.com.wine.domain.model.FaixaCep;
 import br.com.wine.domain.repository.FaixaCepRepository;
@@ -42,6 +45,16 @@ public class FaixaCepController {
 			return ResponseEntity.ok(faixaCepSalva);
 		} catch (FaixaCepConflitoException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> excluir(@PathVariable Long id) {
+		try {
+			faixaCepService.excluir(id);
+			return ResponseEntity.noContent().build();
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.notFound().build();
 		}
 	}
 }
--- src/main/java/br/com/wine/domain/service/FaixaCepService.java
+++ src/main/java/br/com/wine/domain/service/FaixaCepService.java
@@ -3,20 +3,27 @@ package br.com.wine.domain.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.wine.domain.exception.EntidadeNaoEncontradaException;
 import br.com.wine.domain.exception.FaixaCepConflitoException;
 import br.com.wine.domain.model.FaixaCep;
 import br.com.wine.domain.repository.FaixaCepRepository;
 
 @Service
 public class FaixaCepService {
-	
+
 	@Autowired
 	private FaixaCepRepository faixaCepRepository;
-	
+
 	public FaixaCep salvar(FaixaCep faixaCep) throws FaixaCepConflitoException {
 		if (faixaCepRepository.jaPossuiFaixa(faixaCep)) {
 			throw new FaixaCepConflitoException("As faixas inicial ou final já foram cadastradas");
 		}
 		return faixaCepRepository.save(faixaCep);
+	}
+
+	public void excluir(Long id) throws EntidadeNaoEncontradaException {
+		final FaixaCep faixaCep = faixaCepRepository.findById(id)
+				.orElseThrow(() -> new EntidadeNaoEncontradaException("Faixa Cep não encontrada"));
+		faixaCepRepository.delete(faixaCep);
 	}
 }
